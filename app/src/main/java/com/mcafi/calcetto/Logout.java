98f8f7847f3afb440af8602392a32583a2646dae
enrico <enrico.porcile.iic97@gmail.com>
2020-05-21 11:19:24 +0200

    Login e registrazione firebase

---
+++ app/src/main/java/com/mcafi/calcetto/Logout.java
@@ -0,0 +1,27 @@
+package com.mcafi.calcetto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Logout extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+        Button Logout= findViewById(R.id.logout);
+        Logout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(Logout.this,Login.class));
+    }
+}
