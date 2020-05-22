cb9ef24253e3ca8a64b61467837e9f8ce2f30057
mcafi <matteo.carniglia@gmail.com>
2020-05-22 18:07:22 +0200

    aggiunto bottone per tornare indietro

--- app/src/main/java/com/mcafi/calcetto/NewMatchActivity.java
+++ app/src/main/java/com/mcafi/calcetto/NewMatchActivity.java
@@ -1,6 +1,7 @@
 package com.mcafi.calcetto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,15 @@ public class NewMatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_match);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
