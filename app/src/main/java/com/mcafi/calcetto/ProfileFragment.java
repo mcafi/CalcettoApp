f040c73242c3ea72349ea251b8e84dd95cd33a74
loren <lorenzo.grattarola@gmail.com>
2020-05-23 16:25:29 +0200

    update colori + logout button in profilo

--- app/src/main/java/com/mcafi/calcetto/ProfileFragment.java
+++ app/src/main/java/com/mcafi/calcetto/ProfileFragment.java
@@ -1,17 +1,34 @@
 package com.mcafi.calcetto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-public class ProfileFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class ProfileFragment extends Fragment  implements View.OnClickListener{
+
+    private View v;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_profile, viewGroup, false);
+
+        v = inflater.inflate(R.layout.fragment_profile, viewGroup, false);
+        Button Logout = v.findViewById(R.id.logout);
+        Logout.setOnClickListener(this);
+        return v;
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getActivity(), LoginActivity.class));
     }
 
 }
