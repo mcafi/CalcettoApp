eef767ead71ea8636efc84e1b9cb199c0d550316
mcafi <matteo.carniglia@gmail.com>
2020-05-24 18:49:44 +0200

    working settings page

--- app/src/main/java/com/mcafi/calcetto/ProfileFragment.java
+++ app/src/main/java/com/mcafi/calcetto/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.mcafi.calcetto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +17,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
-public class ProfileFragment extends Fragment  implements View.OnClickListener  {
+import javax.annotation.Nullable;
+
+public class ProfileFragment extends Fragment implements View.OnClickListener  {
 
     private View v;
     private FirebaseAuth mAuthReg = FirebaseAuth.getInstance();
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseFirestore db = initializeDatabase();
     private FirebaseUser firebaseUser;
     private User user;
 
@@ -30,11 +36,13 @@ public class ProfileFragment extends Fragment  implements View.OnClickListener  
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
         v = inflater.inflate(R.layout.fragment_profile, viewGroup, false);
         firebaseUser = mAuthReg.getCurrentUser();
+
         final TextView nameText = v.findViewById(R.id.nameText);
         final TextView usernameText = v.findViewById(R.id.usernameText);
         final TextView emailText = v.findViewById(R.id.emailText);
-        final DocumentReference docRef = db.collection("utenti").document(firebaseUser.getUid());
-        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+
+        final DocumentReference userRef = db.collection("utenti").document(firebaseUser.getUid());
+        userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 User user = documentSnapshot.toObject(User.class);
@@ -43,6 +51,20 @@ public class ProfileFragment extends Fragment  implements View.OnClickListener  
                 emailText.setText(user.getEmail());
             }
         });
+
+        userRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (snapshot != null && snapshot.exists()) {
+                    User user = snapshot.toObject(User.class);
+                    nameText.setText(user.getName());
+                    usernameText.setText(user.getUsername());
+                    emailText.setText(user.getEmail());
+                }
+            }
+        });
+
         Button logout = v.findViewById(R.id.logoutButton);
         logout.setOnClickListener(this);
         FloatingActionButton settingsButton = v.findViewById(R.id.settingsButton);
@@ -63,6 +85,14 @@ public class ProfileFragment extends Fragment  implements View.OnClickListener  
                 startActivity(new Intent(getActivity(), SettingsActivity.class));
                 break;
         }
+    }
 
+    private FirebaseFirestore initializeDatabase() {
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.setFirestoreSettings(settings);
+        return db;
     }
 }
--- app/src/main/java/com/mcafi/calcetto/SettingsActivity.java
+++ app/src/main/java/com/mcafi/calcetto/SettingsActivity.java
@@ -3,24 +3,80 @@ package com.mcafi.calcetto;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuthReg = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = initializeDatabase();
+    private FirebaseUser firebaseUser = mAuthReg.getCurrentUser();
+    private final DocumentReference userRef = db.collection("utenti").document(firebaseUser.getUid());
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.settingsToolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        final TextView nameText = findViewById(R.id.nameText);
+        final TextView usernameText = findViewById(R.id.usernameText);
+        TextView saveSettings = findViewById(R.id.saveSettings);
+        saveSettings.setOnClickListener(this);
+
+        userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User user = documentSnapshot.toObject(User.class);
+                nameText.setText(user.getName());
+                usernameText.setText(user.getUsername());
+            }
+        });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView nameText = findViewById(R.id.nameText);
+        TextView usernameText = findViewById(R.id.usernameText);
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", nameText.getText().toString());
+        user.put("username", usernameText.getText().toString());
+        userRef.update(user);
+        Toast.makeText(getApplicationContext(), "Modifiche salvate!", Toast.LENGTH_LONG);
+    }
+
+    private FirebaseFirestore initializeDatabase() {
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.setFirestoreSettings(settings);
+        return db;
     }
 }
