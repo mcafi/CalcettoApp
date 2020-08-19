b9a91d997eeff6f190012352509457f87cb39aff
mcafi <matteo.carniglia@gmail.com>
2020-08-19 11:53:48 +0200

    aggiunto place

--- app/src/main/java/com/mcafi/calcetto/NewMatchActivity.kt
+++ app/src/main/java/com/mcafi/calcetto/NewMatchActivity.kt
@@ -3,9 +3,15 @@ package com.mcafi.calcetto
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
@@ -32,10 +38,14 @@ class NewMatchActivity : AppCompatActivity(), View.OnClickListener {
     private val minute = c.get(Calendar.MINUTE)
     private val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("it"))
     private val timeFormat = SimpleDateFormat("HH:mm", Locale("it"))
+    private var matchPlace: Place? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_match)
+
+        Places.initialize(this@NewMatchActivity, "AIzaSyDcEEGrjxw712L4SX0oUWQ0Cgn3BKPAbGE")
+        //val placesClient = Places.createClient(this)
 
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
@@ -56,6 +66,20 @@ class NewMatchActivity : AppCompatActivity(), View.OnClickListener {
             newMatchTime.setText(timeFormat.format(c.time))
         }, hour, minute, true)
 
+        val autocompleteFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                matchPlace = place
+                Log.i("TAG", "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(p0: Status) {
+                // TODO: Handle the error.
+                Log.i("TAG", "An error occurred: $p0")
+            }
+        })
+
         newMatchDate.setOnClickListener(this)
         newMatchTime.setOnClickListener(this)
         saveMatchButton.setOnClickListener(this)
@@ -75,7 +99,7 @@ class NewMatchActivity : AppCompatActivity(), View.OnClickListener {
                 tpd.show()
             }
             R.id.saveMatchButton -> {
-                val match = Match(firebaseUser.uid, DateTime(Calendar.getInstance()), DateTime(c), newMatchNotes.text.toString(), emptyList<String>(), parseInt(availableSpots.text.toString()))
+                val match = Match(firebaseUser.uid, DateTime(Calendar.getInstance()), DateTime(c), newMatchNotes.text.toString(), emptyList<String>(), parseInt(availableSpots.text.toString()), "", matchPlace!!)
                 db.collection("partite").add(match)
             }
         }
--- app/src/main/java/com/mcafi/calcetto/model/Match.kt
+++ app/src/main/java/com/mcafi/calcetto/model/Match.kt
@@ -1,5 +1,6 @@
 package com.mcafi.calcetto.model
 
+import com.google.android.libraries.places.api.model.Place
 import kotlin.collections.ArrayList
 
 class Match(
@@ -9,5 +10,6 @@ class Match(
         var notes: String = "",
         var participants: List<String> = ArrayList<String>(),
         var available: Int = 0,
-        var id: String = "") {
+        var id: String = "",
+        var place: Place) {
 }
