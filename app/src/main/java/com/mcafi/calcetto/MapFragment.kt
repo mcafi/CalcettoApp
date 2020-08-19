70e6644a1072834a33f0718c31b046310a442b63
mcafi <matteo.carniglia@gmail.com>
2020-08-19 17:47:48 +0200

    aggiunti marker

--- app/src/main/java/com/mcafi/calcetto/MapFragment.kt
+++ app/src/main/java/com/mcafi/calcetto/MapFragment.kt
@@ -1,14 +1,24 @@
 package com.mcafi.calcetto
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.mcafi.calcetto.model.Match
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private val db = initializeDatabase()
+
     override fun onCreateView(inflater: LayoutInflater, viewGroup: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_map, viewGroup, false)
         val mMapView = v.findViewById(R.id.mapView) as MapView
@@ -21,6 +31,30 @@ class MapFragment : Fragment() {
             e.printStackTrace()
         }
 
+        mMapView.getMapAsync(this);
+
         return v
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        db.collection("partite")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        val match = document.toObject(Match::class.java)
+                        googleMap.addMarker(MarkerOptions()
+                                .position(LatLng(match.place.lat, match.place.lng))
+                                .title("ciaooo"))
+                    }
+                }
+    }
+
+    private fun initializeDatabase(): FirebaseFirestore {
+        val settings = FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build()
+        val db = FirebaseFirestore.getInstance()
+        db.firestoreSettings = settings
+        return db
     }
 }
