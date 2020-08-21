2513b0fb9b8fc3ff24b348d41201f2f63761f399
loren <lorenzo.grattarola@gmail.com>
2020-08-21 18:09:06 +0200

    partite create profilo

---
+++ app/src/main/java/com/mcafi/calcetto/MatchAdapterProfile.kt
@@ -0,0 +1,51 @@
+package com.mcafi.calcetto
+
+
+
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.mcafi.calcetto.model.Match
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MatchAdapterProfile(context: Context, private val dataSource: ArrayList<Match>): BaseAdapter() {
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private val dateTimeFormat = SimpleDateFormat("dd/MMM/yyyy - HH:mm", Locale("it"))
+    private val c = Calendar.getInstance()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val rowView = convertView ?: inflater.inflate(R.layout.match_item_profile_xml, parent, false)
+        val match = getItem(position) as Match
+
+        val matchDate = rowView.findViewById(R.id.match_date) as TextView
+        val matchAvailable = rowView.findViewById(R.id.match_available) as TextView
+        val matchNotes = rowView.findViewById(R.id.match_notes) as TextView
+
+        c.set(match.matchDate.year, match.matchDate.month, match.matchDate.day, match.matchDate.hour, match.matchDate.minute)
+
+        matchDate.text = dateTimeFormat.format(c.time)
+        matchAvailable.text = (match.available - match.participants.size).toString()
+        matchNotes.text = match.notes
+
+        return rowView
+    }
+
+    override fun getItem(position: Int): Any {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+}
--- app/src/main/java/com/mcafi/calcetto/ProfileFragment.kt
+++ app/src/main/java/com/mcafi/calcetto/ProfileFragment.kt
@@ -6,12 +6,11 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
@@ -20,6 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.mcafi.calcetto.model.Match
 import com.mcafi.calcetto.model.User
 import kotlinx.android.synthetic.main.fragment_profile.*
 
@@ -34,13 +34,13 @@ class ProfileFragment : Fragment(), View.OnClickListener {
     private lateinit var profileIcon: ImageView
     private val storageRef = storage.reference
     private lateinit var immagini: StorageReference
+    private lateinit var listView: ListView
 
     override fun onCreateView(inflater: LayoutInflater, viewGroup: ViewGroup?, savedInstanceState: Bundle?): View? {
         v = inflater.inflate(R.layout.fragment_profile, viewGroup, false)
         firebaseUser = mAuthReg.currentUser!!
         immagini = storageRef.child("immagini_profilo/" + firebaseUser.uid)
 
-        // carico i dati dell'utente dal db
         val userRef = db.collection("utenti").document(firebaseUser.uid)
         userRef.get().addOnSuccessListener { documentSnapshot ->
             writeUserData(documentSnapshot.toObject(User::class.java)!!)
@@ -51,7 +51,6 @@ class ProfileFragment : Fragment(), View.OnClickListener {
                 writeUserData(snapshot.toObject(User::class.java)!!)
             }
         }
-
         v.findViewById<Button>(R.id.logoutButton).setOnClickListener(this)
         v.findViewById<Button>(R.id.editPicturebutton).setOnClickListener(this)
         val settingsButton: FloatingActionButton = v.findViewById(R.id.settingsButton)
@@ -64,6 +63,37 @@ class ProfileFragment : Fragment(), View.OnClickListener {
         }.addOnFailureListener {
             // Handle any errors
         }
+
+
+
+        listView = v.findViewById(R.id.matches_list)
+
+        val matchList = ArrayList<Match>();
+
+        db.collection("partite")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        val match = document.toObject(Match::class.java)
+                        match.id = document.id
+                        if(match.creator == firebaseUser.uid )
+                        matchList.add(match)
+                    }
+                    val adapter = MatchAdapterProfile(context!!, matchList)
+                    listView.adapter = adapter
+                }
+                .addOnFailureListener { exception ->
+                    Log.d("Main", "Error getting documents: ", exception)
+                }
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val match = parent.getItemAtPosition(position) as Match
+            val viewMatchIntent = Intent(activity, MatchViewActivity::class.java)
+            viewMatchIntent.putExtra("MATCH_ID", match.id)
+            startActivity(viewMatchIntent)
+        }
+
+
         return v
     }
 
