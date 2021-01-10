6480762fe1ee0e2568115ba3d429de4793a5c5a3
enrico <enrico.porcile.iic97@gmail.com>
2021-01-10 18:31:13 +0100

    Aggiunto archivio Loading inziale delle partite

--- app/src/main/java/com/mcafi/calcetto/SettingsActivity.kt
+++ app/src/main/java/com/mcafi/calcetto/SettingsActivity.kt
@@ -1,5 +1,8 @@
 package com.mcafi.calcetto
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -19,6 +22,7 @@ class SettingsActivity : AppCompatActivity(), View.OnClickListener {
     private val firebaseUser = mAuthReg.currentUser
     private val userRef = db.collection("utenti").document(firebaseUser!!.uid)
     private lateinit var user: User
+    private var pendingIntent: PendingIntent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +35,18 @@ class SettingsActivity : AppCompatActivity(), View.OnClickListener {
         val usernameText = findViewById<TextView>(R.id.et_settings_username)
         val saveSettings = findViewById<TextView>(R.id.btn_settings_save)
         saveSettings.setOnClickListener(this)
+        activeNot.setOnClickListener(this)
+        disactiveNot.setOnClickListener(this)
 
         userRef.get().addOnSuccessListener { documentSnapshot ->
             user = documentSnapshot.toObject(User::class.java)!!
             nameText.text = user.name
             usernameText.text = user.username
         }
+
+
+        val intent = Intent(this, alarm::class.java)
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -56,6 +66,35 @@ class SettingsActivity : AppCompatActivity(), View.OnClickListener {
             R.id.logoutButton -> {
                 FirebaseAuth.getInstance().signOut()
                 startActivity(Intent(this, LoginActivity::class.java))
+            }
+            R.id.activeNot -> {
+                /*val manager = getSystemService(ALARM_SERVICE) as AlarmManager
+                int intervallo = 10000;
+        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), intervallo, pendingIntent);
+                Toast.makeText(this, "Ok!", Toast.LENGTH_LONG).show()
+                val calendar = Calendar.getInstance()
+                calendar.timeInMillis = System.currentTimeMillis()
+                calendar[Calendar.YEAR] = 2021
+                calendar[Calendar.MONTH] = 1
+                calendar[Calendar.DAY_OF_MONTH] = 10
+                calendar[Calendar.HOUR_OF_DAY] = 17
+                calendar[Calendar.MINUTE] = 22
+
+// setRepeating() lets you specify a precise custom interval--in this case,
+// 20 minutes.
+                manager[AlarmManager.RTC_WAKEUP, calendar.timeInMillis] = pendingIntent
+                Toast.makeText(this, "Attivato!", Toast.LENGTH_LONG).show()*/
+                Toast.makeText(this, "Attivato!", Toast.LENGTH_LONG).show()
+                val manager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                val intervallo = 5000
+
+                manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), intervallo.toLong(), pendingIntent)
+                Toast.makeText(this, "Attivato!", Toast.LENGTH_LONG).show()
+            }
+            R.id.disactiveNot -> {
+                val manager = getSystemService(ALARM_SERVICE) as AlarmManager
+                manager.cancel(pendingIntent)
+                Toast.makeText(this, "Cancellato!a", Toast.LENGTH_SHORT).show()
             }
         }
     }
